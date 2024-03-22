8f1e82beae491d6e138972a9de764f4b0b4f20a7
Gamaliel Mendez <gmendez@apexsystems.com>
2024-03-22 17:22:23 -0600

    fix en declaracion de variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AngularBootCampHomeworksGamalielMendez';
-  value = 'true';
-  active = true;
+  value: string = 'true';
+  active: boolean = true;
   changeColor() {
     this.active = !this.active;
     this.value = this.active ? 'true' : 'false';
