eb5ec744c39a63035bbdfd89926f43ef2034e0a4
Gamaliel Mendez <gmendez@apexsystems.com>
2024-03-22 17:46:19 -0600

    modificacion relacionada a directivas estructurales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularBootCampHomeworksGamalielMendez';
+  arrayOfLetters: Array<string> = ['A', 'B', 'C'];
 }
